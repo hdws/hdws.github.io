675ad557b6128baba12f1408015955f58f66105e
hdws <1722740047@qq.com>
2017-07-12 11:39:17 +0800

    add bdtj

---
+++ js/bdtj.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?37fabebe62ab6e6736175261cff22907";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
