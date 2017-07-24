3147c82d186f841c0137ab5e4c4cde689c00ecb9
kevin-cc <ninghuimail@163.com>
2017-07-24 17:26:38 +0800

    add browser-check

--- js/bdtj.js
+++ js/bdtj.js
@@ -5,3 +5,19 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
+
+// IE
+$(function(){
+    if(navigator.userAgent.indexOf("MSIE")>0){   
+      var r = $(["<div class='browserupdate clearfix'>", "<i></i>您的浏览器版本过低，建议您更新版本从而获得最佳浏览体验！<a href='browser.html' target='_blank'>立即更新</a>", "<p class='right'><a href='javascript:;' class='after'>稍后再说</a><a href='javascript:;' class='close'><i></i></a></p>", "</div>"].join(""));
+      if(navigator.userAgent.indexOf("MSIE 6.0")>0 || navigator.userAgent.indexOf("MSIE 7.0")>0 || navigator.userAgent.indexOf("MSIE 8.0")>0){  
+        $("body").prepend(r);
+        $(".browserupdate .close").click(function() {
+            $(".browserupdate").fadeOut(500);
+        }),
+        $(".browserupdate .after").click(function() {
+            $(".browserupdate").fadeOut(500);
+        })
+      }   
+    } 
+})
