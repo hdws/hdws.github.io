4678fd19fd4ac78b7bf66f47ed1d4fdf8a225c77
kevin-cc <ninghuimail@163.com>
2017-08-22 09:13:13 +0800

    change college page

---
+++ js/college.js
@@ -0,0 +1,43 @@
+new Vue({
+	el: '#scrolltext',
+	data: {
+	    text:'大学生文学作品征稿了！“还读我诗”公众号平台新近推出大学生文学作品专栏，想不想把你自己的故事讲给我们听呢？可将故事以诗歌、小说、散文等形式分享给我们，每篇100元稿酬等你拿哦！投稿邮箱：huanduwoshi2016@126.com，联系方式：Whia2021(个人微信)。'
+	}
+});
+new Vue({
+	el: '#college',
+	data: {
+	    items: [
+	    	{
+				name:'三岔口的主观记事',
+				author:'何婷',
+				url:'http://mp.weixin.qq.com/s/4N6GUiq2t4PNT8bu5t2fOQ',
+				img:'./images/list_img/article62.jpg'
+			},
+	    	{
+				name:'原谅我不懂你的悲伤',
+				author:'我给你写信',
+				url:'http://mp.weixin.qq.com/s/8P2Hn0geelCMVDZCvTQjtQ',
+				img:'./images/list_img/article60.jpg'
+			},
+			{
+				name:'绿豆冰糕',
+				author:'屈月',
+				url:'http://mp.weixin.qq.com/s/9K9NNx1ARfeUMjWSV1ddlg',
+				img:'./images/list_img/article57.jpg'
+			},
+			{
+				name:'不再仰望星空',
+				author:'禾火',
+				url:'http://mp.weixin.qq.com/s/yfk8hJCTaDnqEjG5WBrvTw',
+				img:'./images/list_img/article55.jpg'
+			},
+			{
+				name:'这本书永远都读不完',
+				author:'我给你写信',
+				url:'http://mp.weixin.qq.com/s/kV59muE1fWLXVAlyGm1IrQ',
+				img:'./images/list_img/article54.jpg'
+			},
+		]
+	}
+});
