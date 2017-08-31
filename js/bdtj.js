54676b7abb528aad231aa08c8dabc9b840075dbe
kevin-cc <ninghuimail@163.com>
2017-08-31 19:37:46 +0800

    change brower tips text

--- js/bdtj.js
+++ js/bdtj.js
@@ -9,7 +9,7 @@ var _hmt = _hmt || [];
 // IE
 $(function(){
     if(navigator.userAgent.indexOf("MSIE")>0){   
-      var r = $(["<div class='browserupdate clearfix'>", "<i></i>您的浏览器版本过低，建议您更新版本从而获得最佳浏览体验！<a href='browser.html' target='_blank'>立即更新</a>", "<p class='right'><a href='javascript:;' class='after'>稍后再说</a><a href='javascript:;' class='close'><i></i></a></p>", "</div>"].join(""));
+      var r = $(["<div class='browserupdate clearfix'>", "<i></i>您的浏览器版本实在太低了，建议您立即更新版本从而获得最佳上网体验！<a href='browser.html' target='_blank'>立即更新</a>", "<p class='right'><a href='javascript:;' class='after'>稍后再说</a><a href='javascript:;' class='close'><i></i></a></p>", "</div>"].join(""));
       if(navigator.userAgent.indexOf("MSIE 6.0")>0 || navigator.userAgent.indexOf("MSIE 7.0")>0 || navigator.userAgent.indexOf("MSIE 8.0")>0){  
         $("body").prepend(r);
         $(".browserupdate .close").click(function() {
